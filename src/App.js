b4bf9ac602c9b267c1dc91984986c4a31d850e35
Ikram <ikramhussein7@gmail.com>
2023-09-21 15:01:00 +0100

    browser routes established

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+// app css component
+
 import "./App.css";
+//importing browser routes
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// importing pages
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+
+// app component
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="About">
+            <About />
+          </Route>
+          <Route path="Projects">
+            <Projects />
+          </Route>
+          <Route path="Contact">
+            <Contact />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
